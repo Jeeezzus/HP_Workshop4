067a01be291a7f0b6187022060cd1c04f083faba
Gauthier Gendreau <gauthier.gendreau@outlook.com>
2022-11-23 17:49:12 +0100

    add coments

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ class Item(BaseModel):
           date: str
           desc: str
 
-@app.get("/todo")
+@app.get("/todo") #get all items on the TODOLIST
 async def list():
     return TODOLIST
 
-@app.get("/todo/{item_id}")
+@app.get("/todo/{item_id}") #get specific item datas depending on the id
 async def root(item_id: int):
      it = find_item(item_id)
      return {(str(it.id)) + "; title: " + str(it.title) + ", date: " + str(it.date) + ", state: " + str(it.state) + ", description: " + str(it.desc)}
 
-@app.post("/todo")
+@app.post("/todo") #add a todo with a json input
 async def create_item(item: Item):
      TODOLIST.append(item)
      return item
 
-@app.delete("/todo/{item_id}")
+@app.delete("/todo/{item_id}") #delete specific item
 def delete_todo(item_id: int) -> None:
      item_to_remove = find_item(item_id)
 
@@ -35,7 +35,7 @@ def delete_todo(item_id: int) -> None:
      return item_to_remove
 
 
-def find_item(id):
+def find_item(id): #input id output item
     for it in TODOLIST:
         if it.id == id:
             return it
